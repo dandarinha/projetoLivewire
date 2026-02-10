0c48f31feac5848072c8f97498cfa764da7f98d9
dandarinha <dandarafrancabarbosa@gmail.com>
2026-02-10 15:38:21 -0300

    exemplo Livewire

---
+++ resources/views/livewire/tarefa/create.blade.php
@@ -0,0 +1,29 @@
+<div class="mt-5">
+    <div class="card">
+        <h5 class="card-header">Cadastro de Tarefas</h5>
+        <div class="body">
+            <form wire:submit.prevent="store">
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" class="form-control" wire:model="nome" name="nome" id="nome" placeholder="Ex: Estudar...">
+                </div>
+
+                <div class="mb-3">
+                    <label for="data_hora" class="form-label">Data da Tarefa</label>
+                    <input type="datetime-local" class="form-control" wire:model="data_hora" id="data_hora" name="data_hora">
+                </div>
+
+                <div class="mb-3">
+                    <label for="descricao" class="form-label">Descrição</label>
+                    <textarea class="form-control" wire:model="descricao" id="descricao" name="descricao" rows="4"></textarea>
+                </div>
+
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-success">Salvar</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+    
+</div>
